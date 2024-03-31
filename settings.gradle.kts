8c69a7dd99d84a3ed56824467ed5ef9b3862eed4
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-03-31 19:47:00 +0300

    fix: add settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
 rootProject.name = "lab1-Java-IS-ITMO"
 include("Bank")
 include("BankConsole")
+include("lab2")
+include("lab2:DAO")
+findProject(":lab2:DAO")?.name = "DAO"
+include("lab2:service")
+findProject(":lab2:service")?.name = "service"
+include("lab2:controller")
+findProject(":lab2:controller")?.name = "controller"
+include("lab2:controller")
+findProject(":lab2:controller")?.name = "controller"
