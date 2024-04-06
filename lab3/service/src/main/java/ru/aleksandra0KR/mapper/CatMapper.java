e4c971698d48572c8ed223e7d530f0e6e509bd12
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-04-06 11:06:46 +0300

    feat: add mappers

---
+++ lab3/service/src/main/java/ru/aleksandra0KR/mapper/CatMapper.java
@@ -0,0 +1,26 @@
+package ru.aleksandra0KR.mapper;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import lombok.experimental.UtilityClass;
+import ru.aleksandra0KR.dto.CatDto;
+import ru.aleksandra0KR.dto.PersonDto;
+import ru.aleksandra0KR.entity.Cat;
+import ru.aleksandra0KR.entity.Person;
+import ru.aleksandra0KR.repository.PersonRepository;
+
+@UtilityClass
+public class CatMapper {
+
+  public CatDto asDto(Cat cat) {
+    Objects.requireNonNull(cat);
+    return new CatDto(cat.getId(), cat.getName(), cat.getBirthday(), cat.getBreed(),
+        cat.getColor());
+  }
+
+  public Cat asDao(CatDto cat) {
+    Objects.requireNonNull(cat);
+    return new Cat(cat.getId(), cat.getName(), cat.getColor(), cat.getBreed(), cat.getBirthDate(),
+        null, new ArrayList<>());
+  }
+}
