4ed5c947ae31a6a93032fce5ecb1f9f3b62935e1
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-04-06 11:06:21 +0300

    feat: add dao layer with properties

--- lab3/dao/build.gradle.kts
+++ lab3/dao/build.gradle.kts
@@ -10,14 +10,25 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.6")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.6")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.0.6")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.0.6")
 
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testImplementation("junit:junit:4.13.1")
 
-    implementation("org.springframework.data:spring-data-jpa:3.2.2")
+    compileOnly("org.projectlombok:lombok")
+    implementation("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+
+    runtimeOnly("org.postgresql:postgresql:42.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    implementation("javax.persistence:javax.persistence-api:2.2")
+
+
+
 }
 
 tasks.test {
