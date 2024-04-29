2193f0d8fca06efec02f1852df431c17109fcefa
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-04-29 22:34:00 +0300

    feat: dto, exceptions and mappers from the previous labs

---
+++ lab4/service/src/main/java/ru/aleksandra0KR/mapper/OwnerMapper.java
@@ -0,0 +1,23 @@
+package ru.aleksandra0KR.mapper;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import lombok.experimental.UtilityClass;
+import ru.aleksandra0KR.dto.OwnerDto;
+import ru.aleksandra0KR.model.Owner;
+
+@UtilityClass
+
+public class OwnerMapper {
+
+  public OwnerDto asDto(Owner person) {
+    Objects.requireNonNull(person);
+    return new OwnerDto(person.getOwner_id(), person.getName(), person.getBirthday(), person.getPerson()
+        .getRoles());
+  }
+
+  public Owner asDao(OwnerDto person) {
+    Objects.requireNonNull(person);
+    return new Owner(person.getId(), person.getName(), person.getBirthDate(), new ArrayList<>(), null);
+  }
+}
